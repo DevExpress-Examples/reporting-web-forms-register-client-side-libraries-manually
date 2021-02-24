65b6f9816bbf82f86e9e0964b91361f17ee16796
DevExpressExampleBot <fakeEmail>
2021-02-24 14:54:00 +0300

    Source auto update [skip ci]

---
+++ VB/TestReportDesigner/Default.aspx.vb
@@ -0,0 +1,19 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports TestReportDesigner.Reports
+
+Namespace TestReportDesigner
+	Partial Public Class [Default]
+		Inherits System.Web.UI.Page
+
+		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+			If Not IsPostBack Then
+				reportDesigner.OpenReport(New SampleReport())
+			End If
+		End Sub
+	End Class
+End Namespace
