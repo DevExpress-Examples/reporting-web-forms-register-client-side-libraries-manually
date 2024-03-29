684346f63edc6669f3da7e13aa85917198e4bc0b
DevExpressExampleBot <fakeEmail>
2024-03-29 19:01:33 +0400

    Source auto update [skip ci]

--- VB/T115434/Default.aspx.vb
+++ VB/T115434/Default.aspx.vb
@@ -1,17 +1,15 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Web
+Imports System
 Imports System.Web.UI
 Imports System.Web.UI.WebControls
 Imports DevExpress.XtraReports.UI
 
 Namespace T115434
-	Partial Public Class [Default]
-		Inherits System.Web.UI.Page
 
-		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-			ASPxReportDesigner1.OpenReport(New XtraReport())
-		End Sub
-	End Class
+    Public Partial Class [Default]
+        Inherits Page
+
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ASPxReportDesigner1.OpenReport(New XtraReport())
+        End Sub
+    End Class
 End Namespace
